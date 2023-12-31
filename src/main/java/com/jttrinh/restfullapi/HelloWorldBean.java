03fec7823361208c7c600762bfb433c4db4983cc
jt trinh <Nhattd@selex.vn>
2023-12-31 13:01:59 +0700

    path variablee

---
+++ src/main/java/com/jttrinh/restfullapi/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.jttrinh.restfullapi;
+
+public class HelloWorldBean {
+    private String messsage;
+    public HelloWorldBean(String message) {
+        this.messsage = message;
+    }
+
+    public String getMesssage() {
+        return messsage;
+    }
+
+    public void setMesssage(String messsage) {
+        this.messsage = messsage;
+    }
+
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "messsage='" + messsage + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/jttrinh/restfullapi/HelloWorldController.java
+++ src/main/java/com/jttrinh/restfullapi/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.jttrinh.restfullapi;
-
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //RESTAPI
@@ -15,4 +12,15 @@ public class HelloWorldController {
     public String helloWorld(){
         return "Hello world";
     }
+
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world bean");
+    }
+
+    @GetMapping(path = "path-variable/{name}")
+    public HelloWorldBean pathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
 }
